0320a5227e4480a8de3be93a97a77d44e1d53115
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 13:03:37 -0700

    Faulty input updates

--- game.py
+++ game.py
@@ -9,16 +9,21 @@ name = input("What is your name? ")
 rand_num = randint(1,100)
 
 guess = int(input("Pick a number between 1 and 100. "))
-
 guesses = 1
+
 while guess != rand_num:
-	if guess > rand_num:
+	if guess > 100 or guess < 1:
+		print("You fool, we asked for a number between 1 and 100. Try again.")
+	elif guess > rand_num:
 		print ("Your guess is too high, try again.")
 	else:
 		print ("Your guess is too low, try again.")
 	guesses += 1
 
-	guess = int(input("Your guess? "))
+	try:
+		guess = int(input("Your guess? "))
+	except:
+		print("Please input a valid integer")
 
 print("Well done, " + name + "! It only took you " + str(guesses) + " tries!")
 
