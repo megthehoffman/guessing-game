6e8f6d7eea02e3cec5a718f70a0c1675e3bf6e66
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 16:07:11 -0700

    Added Try and Except

--- game.py
+++ game.py
@@ -7,23 +7,27 @@ print("Hello player!")
 name = input("What is your name? ")
 
 rand_num = randint(1,100)
+print(rand_num)
 
-guess = int(input("Pick a number between 1 and 100. "))
+guess = input("Pick a number between 1 and 100. ")
+
 guesses = 1
 
-while guess != rand_num:
-	if guess > 100 or guess < 1:
-		print("You fool, we asked for a number between 1 and 100. Try again.")
-	elif guess > rand_num:
-		print ("Your guess is too high, try again.")
-	else:
-		print ("Your guess is too low, try again.")
-	guesses += 1
-
+while True:
 	try:
-		guess = int(input("Your guess? "))
+		guess = int(guess)	
+		if guess == rand_num:
+			break
+		elif guess > 100 or guess < 1:
+			print("You fool, we asked for a number between 1 and 100. Try again.")
+		elif guess > rand_num:
+			print ("Your guess is too high, try again.")
+		else:
+			print ("Your guess is too low, try again.")	
 	except:
-		print("Please input a valid integer")
+		print("Please input a valid integer.")
+	guesses += 1
+	guess = input("Your guess? ")
 
 print("Well done, " + name + "! It only took you " + str(guesses) + " tries!")
 
