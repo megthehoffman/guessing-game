7c7f01a43082cb62a8550d6fd6e1bc95bb91e461
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 12:34:21 -0700

    Choose random number

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 """A number-guessing game."""
 
 # Put your code here
+from random import randint
+
 print("Hello player!")
 name = input("What is your name?")
+
+rand_num = randint(1,100)
+print(rand_num)
