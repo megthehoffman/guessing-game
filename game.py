4baca1df55e40e9a670085c6f6af0791fcd216d2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 16:34:34 -0700

    Let user play multiple rounds and save best score

--- game.py
+++ game.py
@@ -3,32 +3,54 @@
 # Put your code here
 from random import randint
 
+def is_matching_number():
+	rand_num = randint(1,100)
+	#print(rand_num)
+
+	guess = input("Pick a number between 1 and 100. ")
+	guesses = 1
+
+	while True:
+		try:
+			guess = int(guess)	
+			if guess == rand_num:
+				break
+			elif guess > 100 or guess < 1:
+				print("You fool, we asked for a number between 1 and 100. Try again.")
+			elif guess > rand_num:
+				print ("Your guess is too high, try again.")
+			else:
+				print ("Your guess is too low, try again.")	
+		except:
+			print("Please input a valid integer.")
+		
+		guesses += 1
+		guess = input("Your guess? ")
+
+	print("Well done, " + name + "! It only took you " + str(guesses) + " tries!")
+
+	return guesses
+
 print("Hello player!")
 name = input("What is your name? ")
 
-rand_num = randint(1,100)
-print(rand_num)
-
-guess = input("Pick a number between 1 and 100. ")
-
-guesses = 1
+best_score = is_matching_number()
 
 while True:
-	try:
-		guess = int(guess)	
-		if guess == rand_num:
-			break
-		elif guess > 100 or guess < 1:
-			print("You fool, we asked for a number between 1 and 100. Try again.")
-		elif guess > rand_num:
-			print ("Your guess is too high, try again.")
-		else:
-			print ("Your guess is too low, try again.")	
-	except:
-		print("Please input a valid integer.")
-	guesses += 1
-	guess = input("Your guess? ")
+	play_again = input("Do you want to play again? Enter Y or N. ")
+	play_again = play_again.lower()
 
-print("Well done, " + name + "! It only took you " + str(guesses) + " tries!")
+	if play_again == 'y':
+		score = is_matching_number()
+		if score < best_score:
+			best_score = score
+			print("New high score, " + name + "! " +str(best_score) + " tries")
+	elif play_again == 'n':
+		break
+	else:
+		print("Please enter valid input.")
+
+print("Thanks for playing, " + name + "! Your best score was " + str(best_score) + " tries.")
+
 
 
