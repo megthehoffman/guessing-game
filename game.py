838f8290c0bd09eba8d756ec0b41868a35d37f6f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 12:47:14 -0700

    Add While Statement for Guessing

--- game.py
+++ game.py
@@ -4,7 +4,22 @@
 from random import randint
 
 print("Hello player!")
-name = input("What is your name?")
+name = input("What is your name? ")
 
 rand_num = randint(1,100)
-print(rand_num)
+
+guess = int(input("Pick a number between 1 and 100. "))
+
+guesses = 1
+while guess != rand_num:
+	if guess > rand_num:
+		print ("Your guess is too high, try again.")
+	else:
+		print ("Your guess is too low, try again.")
+	guesses += 1
+
+	guess = int(input("Your guess? "))
+
+print("Well done, " + name + "! It only took you " + str(guesses) + " tries!")
+
+
