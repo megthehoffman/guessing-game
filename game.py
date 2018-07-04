1d7c667cc239d8c58c08a3ecd5fdac04f1866790
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 17:33:16 -0700

    Limited Number of Guesses and Added Range Option

--- game.py
+++ game.py
@@ -4,19 +4,25 @@
 from random import randint
 
 def is_matching_number():
-	rand_num = randint(1,100)
+	""" Play game to guess matching number"""
+	a = int(input("You are going to guess an integer in a range, but you get to pick the range! What would you like the lower bound to be? "))
+	b = int(input("What would you like the upper bound to be? "))
+
+	rand_num = randint(a,b)
 	#print(rand_num)
 
-	guess = input("Pick a number between 1 and 100. ")
-	guesses = 1
+	guesses = 0
+	guess = input("Pick a number between " + str(a) + " and " + str(b) + ".")
 
 	while True:
+		guesses += 1
 		try:
 			guess = int(guess)	
 			if guess == rand_num:
+				game_won = True
 				break
-			elif guess > 100 or guess < 1:
-				print("You fool, we asked for a number between 1 and 100. Try again.")
+			elif guess > b or guess < a:
+				print("You fool, we asked for a number between " + str(a) + " and " + str(b) + ". Try again. ")
 			elif guess > rand_num:
 				print ("Your guess is too high, try again.")
 			else:
@@ -24,12 +30,24 @@ def is_matching_number():
 		except:
 			print("Please input a valid integer.")
 		
-		guesses += 1
+		
+
+		if guesses >= 10:
+			print("Too many tries! Game over. Your score is 0.")
+			game_won = False
+			break
+
+		print("You have " + str(10-guesses) + " remaining.")
 		guess = input("Your guess? ")
 
-	print("Well done, " + name + "! It only took you " + str(guesses) + " tries!")
+	if game_won == True:
+		print("Well done, " + name + "! It only took you " + str(guesses) + " tries!")
+		converted_score = int(guesses/(10/(b-a)))
+		print("Your score is " + str(converted_score) + ".")
+	else:
+		converted_score = 0
 
-	return guesses
+	return converted_score
 
 print("Hello player!")
 name = input("What is your name? ")
@@ -42,15 +60,15 @@ while True:
 
 	if play_again == 'y':
 		score = is_matching_number()
-		if score < best_score:
+		if score > best_score:
 			best_score = score
-			print("New high score, " + name + "! " +str(best_score) + " tries")
+			print("New high score, " + name + "! Your score is " + str(best_score) + " points.")
 	elif play_again == 'n':
 		break
 	else:
 		print("Please enter valid input.")
 
-print("Thanks for playing, " + name + "! Your best score was " + str(best_score) + " tries.")
+print("Thanks for playing, " + name + "! Your best score was " + str(best_score) + " points.")
 
 
 
